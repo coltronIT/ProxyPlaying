4fa9277eb04e15a7ecf3f3cde2c14538f834386d
Colton Duncan <coltronit@gmail.com>
2023-11-21 10:58:18 -0500

    fixed output for flask

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,6 +1,8 @@
-import json
 from time import sleep
-from flask import Flask
+from flask import (
+    Flask,
+    jsonify,
+)
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,11 +12,3 @@ CORS(app)
 @app.route('/api/all_songs')
 def get_song_data():
     sleep(5)
-    return json.dumps({
-        'songs':
-            {
-                '1': 'Cruel Summer: 5%',
-                '2': 'Anti-Hero: 20%',
-                '3': 'Lovestory: 100%'
-            }
-    })
