20b09cc483555910a5578901568f8d708b25fa61
sandtomb <minusben@gmail.com>
2023-11-25 22:10:13 -0500

    Update all_songs endpoint

--- flask-app/app.py
+++ flask-app/app.py
@@ -12,3 +12,20 @@ CORS(app)
 @app.route('/api/all_songs')
 def get_song_data():
     sleep(5)
+
+    songs = [
+        {
+            'id': '1',
+            'songDisplay': 'Cruel Summer - 10%',
+        },
+        {
+            'id': '2',
+            'songDisplay': 'Anti-Hero - 20%',
+        },
+        {
+            'id': '3',
+            'songDisplay': 'Lovestory - 100%',
+        }
+    ]
+
+    return jsonify(songs)
